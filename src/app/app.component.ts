aab26a3a7b11abae5a1dc5cb477c39f47ca9c0c0
Mohamed-Talaat <eng.muhamedtalaat@gmail.com>
2023-11-07 13:18:22 +0200

    Finish TO DO Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
+interface todoItems{
+  value: string,
+  checked:boolean
+  }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+  
 export class AppComponent {
   title = 'angularInteview';
+  todoForm: FormGroup = new FormGroup({
+    item: new FormControl(null),
+  });
+
+  items: todoItems[] = [
+    { value: 'list1', checked: true },
+    { value: 'list2', checked: false }
+  ];
+
+  addNewToDoItem() {
+    console.log(this.todoForm.value?.item);
+    if (this.todoForm.value?.item !== null ) {
+      this.items.push({ value: this.todoForm.value?.item, checked: false });
+    }
+  }
+
 }
